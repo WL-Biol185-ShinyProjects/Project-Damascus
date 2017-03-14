1cc42c5f034c3afe927d18dc87d2ace9e222621d
Chris Myers <myersc17@mail.wlu.edu>
2017-03-14 12:57:18 -0400

    Code for converting Lat and Long Stupid Code about clean data

--- Convert Address to Long and Lat.R
+++ Convert Address to Long and Lat.R
@@ -1,15 +1,6 @@
-geocodeAddress <- function(address) {
-  require(RJSONIO)
-  url <- "http://maps.google.com/maps/api/geocode/json?address="
-  url <- URLencode(paste(url, address, "&sensor=false", sep = ""))
-  x <- fromJSON(url, simplify = FALSE)
-  if (x$status == "OK") {
-    out <- c(x$results[[1]]$geometry$location$lng,
-             x$results[[1]]$geometry$location$lat)
-  } else {
-    out <- NA
-  }
-  Sys.sleep(0.2)  # API only allows 5 requests per second
-  out
-}
-geocodeAddress("06426")
+library(RDSTK)
+library(ggmap)
+
+lon <- geocode(Clean_Hospital_Data$Street_Address[1:100])
+Clean_Hospital_Data$Longitude <- lon[1]
+Clean_Hospital_Data$Latitude <- lon[2]
