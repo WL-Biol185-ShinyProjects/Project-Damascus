72e29929e7f7ef35a4d9939701988b6b6ecc6ba2
Thomas Caldwell <tcaldwell121295@gmail.com>
2017-03-29 16:43:56 -0400

    Map Works!

--- ThomasConverting Leaflet Trial to Script.R
+++ ThomasConverting Leaflet Trial to Script.R
@@ -9,11 +9,11 @@ library(readr)
 ##money_right <- read_csv("~/Project-Damascus/data/money-right.csv")
 #calling all packages and datasets necessary for app
 ##combined <- read.csv("~/Project-Damascus/data/combined.csv")
-hospitals <- data.frame(lat = Mapping$lat,
-                        lon = Mapping$lon,
-                        place = Mapping$name,
-                        condition = money_right$`DRG Definition`,
-                        state = money_right$`Provider State`,
+hospitals <- data.frame(lat = combined$lat,
+                        lon = combined$lon,
+                        place = combined$name,
+                        condition = combined$DRG.Definition,
+                        state = combined$Provider.State,
                         ##payment = combined$Average.Total.Payments,
                         ##coverage = combined$Average.Covered.Charges,
                         ##medicare_pay = combined$Average.Medicare.Payments,
@@ -25,25 +25,19 @@ ui <- fluidPage(
       tags$strong(style="font-family: Impact", "Hospital Locator")
     )),
     column(8,
-           hospitals %>%
-             filter(`DRG Definition` %in% input$condition) %>%
-             filter(`Provider State` %in% input$state) %>%
-              leaflet() %>% 
-                 setView(lng = -79.442778, lat = 37.783889, zoom = 1) %>%
-                 addTiles() %>%
-                 addCircleMarkers(popup = ~place, clusterOptions = markerClusterOptions())),
+           leafletOutput(outputId = "map")),
            
     column(4,
       inputPanel(
         tags$h4("Please select your", tags$em(tags$strong("Condition")), "and", tags$em(tags$strong("State")), "using the dropdown menus below"),
   # Copy the line below to make a select box 
   selectInput("condition", label = h3("Condition"), 
-              choices = unique(money_right$`DRG Definition`), 
+              choices = unique(hospitals$condition), 
               selected = 1),
   
   # Copy the line below to make a select box 
   selectInput("state", label = h3("State"), 
-              choices = unique(money_right$`Provider State`), 
+              choices = unique(hospitals$state), 
               selected = 1),
   
   checkboxGroupInput("checkGroup", label = h3("Checkbox group"),
@@ -60,10 +54,19 @@ fluidRow(
 #this is just creating space for the graph when we make it.
 server <- function(input,output) {
   output$bar <- renderPlot({
-    money_right %>%
-      filter(`DRG Definition` %in% input$condition) %>%
-      filter(`Provider State` %in% input$state) %>%
-      ggplot(aes(`Provider Name`, `Average Total Payments`, fill = `Provider Name`)) + geom_bar(stat = "identity") + theme(axis.text.x = element_blank()) 
+    combined %>%
+      filter(DRG.Definition %in% input$condition) %>%
+      filter(Provider.State %in% input$state) %>%
+      ggplot(aes(name, Average.Total.Payments, fill = name)) + geom_bar(stat = "identity") + theme(axis.text.x = element_blank()) 
+  })
+  output$map <- renderLeaflet({
+    hospitals %>%
+      filter(condition %in% input$condition) %>%
+      filter(state %in% input$state) %>%
+      leaflet() %>% 
+      setView(lng = -79.442778, lat = 37.783889, zoom = 1) %>%
+      addTiles() %>%
+      addCircleMarkers(popup = ~place, clusterOptions = markerClusterOptions())
   })
 }
   shinyApp(ui=ui, server=server)
